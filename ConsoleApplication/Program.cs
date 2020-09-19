b05165f42384b54f3238a6b51c667a0226d19558
Francisco Gongora <frgongora@byui.edu>
2020-09-19 17:45:15 -0500

    Complete Assingment W01

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//CIT 365
+//Francisco Gongora
+//W01 Assignment
+//September 19th, 2020
+using System;
 
 namespace ConsoleApplication
 {
@@ -6,7 +10,38 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bye World");
+            String myName = "Francisco Gongora";
+            String myLocation = "Playa del Carmen, Mexico";
+            String today = DateTime.UtcNow.ToShortDateString();
+            DateTime christmasDate = new DateTime(2020, 12, 25);
+            int daysForChristmas = (christmasDate - DateTime.UtcNow).Days;
+
+            Console.WriteLine("Hello I am " + myName);
+            Console.WriteLine("I am from: " + myLocation);
+            Console.WriteLine("The current date is " + today);
+            Console.WriteLine("Number of days left for Christmas: " + daysForChristmas);
+
+            //Program from Book 2.1
+
+            Console.WriteLine("This part of the program calculates the Wood Lenght and Glass Area for a window.");
+            double width, height, woodLength, glassArea;
+            String widthString, heightString;
+
+            Console.WriteLine("Enter the width of the Window (in feet)");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.WriteLine("Enter the height of the Window in (in meters)");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.5;
+            glassArea = 2 * (width + height);
+
+            Console.WriteLine("The lenght of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square meters");
+            Console.Write("Pres <Enter> to exit...");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
     }
 }
